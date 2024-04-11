ee9b1546ff85bb92f9861a0cbbfd5f18ad91a3b2
Adrian <moldovan.adrian.alexandru03@gmail.com>
2024-04-12 02:02:17 +0300

    Added an level panel when pressing the play button for chosing the wanted level.

---
+++ Assets/Scripts/ButtonUI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonUI : MonoBehaviour
+{
+    //public Button[] buttons;
+    //private void Awake()
+    //{
+    //    int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel1", 1);
+    //    for(int i = 0; i < buttons.Length; i++)
+    //    {
+    //        buttons[i].interactable = false;
+    //    }
+    //    for (int i = 0; i < unlockedLevel; i++)
+    //    {
+    //        buttons[i].interactable = true;
+    //    }
+    //}
+    public void openLevel(int levelId)
+    {
+        string levelName = "Level" + levelId;
+        SceneManager.LoadScene(levelName);
+    }
+}
